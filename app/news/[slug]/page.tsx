9a395155e9f4abf47599f97f72f77cc68188b6aa
kota_shiratori <swan.kouta@gmail.com>
2025-05-06 21:29:09 +0900

    ⚡️

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,12 +4,7 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-type Props = {
-  params: { slug: string };
-  searchParams?: { dk?: string };
-};
-
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({ params, searchParams }: { params: { slug: string }; searchParams?: { dk?: string } }) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams?.dk,
   }).catch(notFound);
