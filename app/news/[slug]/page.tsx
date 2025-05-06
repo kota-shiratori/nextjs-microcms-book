8ed1a51dd0cb9f6da3834124759d924c0caf6e17
kota_shiratori <swan.kouta@gmail.com>
2025-05-06 21:31:32 +0900

    🐛

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,12 +1,21 @@
-import { notFound } from "next/navigation";
-import { getNewsDetail } from "@/app/_libs/microcms";
-import Article from "@/app/_components/Article";
-import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
+import { notFound } from 'next/navigation';
+import { getNewsDetail } from '@/app/_libs/microcms';
+import Article from '@/app/_components/Article';
+import ButtonLink from '@/app/_components/ButtonLink';
+import styles from './page.module.css';
 
-export default async function Page({ params, searchParams }: { params: { slug: string }; searchParams?: { dk?: string } }) {
+type Props = {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    dk?: string;
+  };
+};
+
+export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams?.dk,
+    draftKey: searchParams.dk,
   }).catch(notFound);
 
   return (
