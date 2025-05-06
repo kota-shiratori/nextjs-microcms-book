6f4029e10992b6ffd3255c4ab07720b80ed4b655
kota_shiratori <swan.kouta@gmail.com>
2025-05-06 17:33:30 +0900

    feat: Chapter5完

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 import Category from '../Category';
 import Date from '../Date';
 import { News } from '@/app/_libs/microcms';
@@ -18,9 +18,24 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          {/* TODO:別の章で、リンクと正式な画像に差し替えます */}
           <Link href={`/news/${article.id}`} className={styles.link}>
-            <Image className={styles.image} src="/no-image.png" alt="No Image" width={1200} height={630} />
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
